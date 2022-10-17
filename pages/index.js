133e995b7a87cabf6faebfdd2997ee7a5b83af48
sarahyu12 <76453986+sarahyu12@users.noreply.github.com>
2022-10-17 18:58:26 -0400

    from replit

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,17 @@ export default function Home() {
         <p className="description">
           Get sty editing <code>pages/index.js</code>
         </p>
+        <h1><center>Calculator App</center></h1>
+  <p>Change:</p>
+  20 dollar bills:<p id="20">0</p>
+  10 dollar bills:<p id="10">0</p>
+  5 dollar bills:<p id="5">0</p>
+  1 dollar bills:<p id="1">0</p>
+  Quarters:<p id="0.25">0</p>
+  Dimes:<p id="0.1">0</p>
+  Nickels:<p id="0.05">0</p>
+  Pennies:<p id="0.01">0</p>
+  <button class="clickme">click me</button>
       </main>
 
       <Footer />
